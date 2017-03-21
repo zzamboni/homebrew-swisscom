34c12bb89b6acce6492fc486f3fb820b794511be
Diego Zamboni <diego.zamboni@swisscom.com>
2017-03-21 16:25:13 +0100

    First commit

---
+++ Casks/epichrome-swisscom-jira.rb
@@ -0,0 +1,10 @@
+cask 'epichrome-swisscom-jira' do
+  version 'latest'
+  sha256 'f63e747c6f63164b6fe1369f50437031a4b22f1e1e3fe21278f925e97b29b219'
+
+  url 'https://'
+  name 'Swisscom Jira site-specific browser'
+  homepage 'https://gitlab.swisscloud.io/zzamboni/homebrew-swisscom'
+
+  app 'Swisscom Jira.app'
+end
