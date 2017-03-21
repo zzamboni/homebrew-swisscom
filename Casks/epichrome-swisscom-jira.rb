0ddb3e75b3dd0677f8ebd93c1add9acdf30744a5
Diego Zamboni <diego.zamboni@swisscom.com>
2017-03-21 21:19:46 +0100

    Fixed URL for github

--- Casks/epichrome-swisscom-jira.rb
+++ Casks/epichrome-swisscom-jira.rb
@@ -2,7 +2,7 @@ cask 'epichrome-swisscom-jira' do
   version 'latest'
   sha256 'f63e747c6f63164b6fe1369f50437031a4b22f1e1e3fe21278f925e97b29b219'
 
-  url 'https://gitlab.swisscloud.io/zzamboni/homebrew-swisscom/raw/master/resources/swisscom-jira-ssb.dmg'
+  url 'https://github.com/zzamboni/homebrew-swisscom/raw/master/resources/swisscom-jira-ssb.dmg'
   name 'Swisscom Jira site-specific browser'
   homepage 'https://gitlab.swisscloud.io/zzamboni/homebrew-swisscom'
 
