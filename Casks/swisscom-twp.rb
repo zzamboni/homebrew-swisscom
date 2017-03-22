96ae1c51dafe24bee687ef894a42cb07680f0eff
Diego Zamboni <diego.zamboni@swisscom.com>
2017-03-22 15:48:03 +0100

    Added swisscom-twp

---
+++ Casks/swisscom-twp.rb
@@ -0,0 +1,10 @@
+cask 'swisscom-twp' do
+  version 'latest'
+  sha256 'ffe911bbe6e990f95010a92cca255e7c9778e2b262a9ddf62552202bde3a9244'
+
+  url 'https://github.com/zzamboni/homebrew-swisscom/raw/master/resources/swisscom-twp-ssb.dmg'
+  name 'Swisscom TWP site-specific browser'
+  homepage 'https://gitlab.swisscloud.io/zzamboni/homebrew-swisscom'
+
+  app 'Swisscom TWP.app'
+end
