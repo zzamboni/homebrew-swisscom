760f987c27b511b9df39c89b6068a731fba597aa
Diego Zamboni <diego.zamboni@swisscom.com>
2017-03-22 15:52:33 +0100

    Added swisscom-smca

---
+++ Casks/swisscom-smca.rb
@@ -0,0 +1,10 @@
+cask 'swisscom-smca' do
+  version 'latest'
+  sha256 'b0f4e85a3e5385bc25aa0ff892d686f15df8cc669be9bfa94c46a82a36a54923'
+
+  url 'https://github.com/zzamboni/homebrew-swisscom/raw/master/resources/swisscom-smca-ssb.dmg'
+  name 'Swisscom SMCA site-specific browser'
+  homepage 'https://gitlab.swisscloud.io/zzamboni/homebrew-swisscom'
+
+  app 'Swisscom SMCA.app'
+end
