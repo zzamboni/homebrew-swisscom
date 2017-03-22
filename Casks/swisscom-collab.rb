cedfe42782bd64b138a5901b2a90b6d73aca6025
Diego Zamboni <diego.zamboni@swisscom.com>
2017-03-22 15:33:31 +0100

    Added swisscom-collab

---
+++ Casks/swisscom-collab.rb
@@ -0,0 +1,10 @@
+cask 'swisscom-collab' do
+  version 'latest'
+  sha256 '972cd1a49177148f17408d249f2bdf99f57fb174e9b00460f5424d22f999df45'
+
+  url 'https://github.com/zzamboni/homebrew-swisscom/raw/master/resources/swisscom-collab-ssb.dmg'
+  name 'Swisscom Collab site-specific browser'
+  homepage 'https://gitlab.swisscloud.io/zzamboni/homebrew-swisscom'
+
+  app 'Swisscom Collab.app'
+end
